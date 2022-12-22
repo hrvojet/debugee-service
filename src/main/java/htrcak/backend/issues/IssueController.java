143e6ba323af3ecd211545c56a22fed811f008df
Hrvoje <hrvojetrcak@gmail.com>
2022-12-22 20:03:43 +0100

    Add GET for Issue controller

--- src/main/java/htrcak/backend/issues/IssueController.java
+++ src/main/java/htrcak/backend/issues/IssueController.java
@@ -1,4 +1,20 @@
 package htrcak.backend.issues;
 
+import htrcak.backend.issues.data.IssueDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("issues")
 public class IssueController {
+
+    private final IssueService issueService;
+
+    public IssueController(IssueService issueService) { this.issueService = issueService; }
+
+    @GetMapping
+    public List<IssueDTO> findAllIssues() { return issueService.findAll(); }
 }
